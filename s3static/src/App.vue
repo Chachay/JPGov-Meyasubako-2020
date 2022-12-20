27a96b5879e0015553908fce021df6abc95598fd
Chachay <git@chachay.org>
2022-12-20 23:12:25 +0900

    Update

--- s3static/src/App.vue
+++ s3static/src/App.vue
@@ -30,7 +30,7 @@
         <b-card-group deck>
           <b-card title="更新情報">
             <b-card-sub-title class="mb-2">サイト情報更新日</b-card-sub-title>
-            <b-card-text class="mx-3">2022/10/19</b-card-text>
+            <b-card-text class="mx-3">2022/12/20</b-card-text>
 
             <b-row>
               <b-col cols="4">
